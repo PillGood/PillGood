9a928367d305eafc362d0e1e63de1f07ff21a395
gayeon1025 <gayeon1559@gmail.com>
2018-06-29 03:09:58 +0900

    Add activity for enrollment of medication

---
+++ app/src/main/java/com/example/pc/pillgood/EnrollMediWithQRActivity.java
@@ -0,0 +1,14 @@
+package com.example.pc.pillgood;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class EnrollMediWithQRActivity extends AppCompatActivity{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_enrollment);
+    }
+}
