b47217ba0e2a811e08544247b4418c8fc254d404
gayeon1025 <gayeon1559@gmail.com>
2018-06-30 07:09:09 +0900

    Enrollment Activity : add push alarm function

---
+++ app/src/main/java/com/example/pc/pillgood/PushAlarmBroadcast.java
@@ -0,0 +1,32 @@
+package com.example.pc.pillgood;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+public class PushAlarmBroadcast extends BroadcastReceiver {
+    String INTENT_ACTION = Intent.ACTION_BOOT_COMPLETED;
+
+    SharedPreferences preferences;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        //throw new UnsupportedOperationException("Not yet implemented");
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification.Builder builder = new Notification.Builder(context);
+        Log.d("*******Disease name", intent.getStringExtra("diseaseName"));
+        builder.setSmallIcon(R.drawable.icon_push).setTicker("HETT").setWhen(System.currentTimeMillis())
+                .setNumber(1).setContentTitle("약을 먹을 시간입니다!").setContentText("감기약을 먹을 시간입니다")
+                .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE).setContentIntent(pendingIntent).setAutoCancel(true);
+
+        notificationManager.notify(1, builder.build());
+    }
+}
