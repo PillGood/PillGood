0933ed97e054e173fde392dbab43b68c318c6b64
Taeyoon Kim <tykimseoul@gmail.com>
2018-06-29 15:38:54 +0900

    map update

--- app/src/main/java/com/example/pc/pillgood/MapActivity.java
+++ app/src/main/java/com/example/pc/pillgood/MapActivity.java
@@ -1,24 +1,60 @@
 package com.example.pc.pillgood;
 
+import android.annotation.SuppressLint;
+import android.content.IntentSender;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.nhn.android.maps.NMapActivity;
 import com.nhn.android.maps.NMapController;
 import com.nhn.android.maps.NMapView;
 import com.nhn.android.maps.maplib.NGeoPoint;
 import com.nhn.android.maps.nmapmodel.NMapError;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 public class MapActivity extends NMapActivity implements NMapView.OnMapStateChangeListener {
 
     // API-KEY
-    public static final String API_KEY = "jpHy_q4keXPvX7KB2ZYF";  //<---맨위에서 발급받은 본인 ClientID 넣으세요.
+    public static final String API_KEY = "jpHy_q4keXPvX7KB2ZYF";
+    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 1000;
+    private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = 500;
+    private static final String TAG = MapActivity.class.getSimpleName();
+    private static final int REQUEST_CHECK_SETTINGS = 100;
     // 네이버 맵 객체
     NMapView mMapView = null;
     // 맵 컨트롤러
     NMapController mMapController = null;
     // 맵을 추가할 레이아웃
     LinearLayout mapContainer;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private SettingsClient mSettingsClient;
+    private LocationRequest mLocationRequest;
+    private LocationSettingsRequest mLocationSettingsRequest;
+    private LocationCallback mLocationCallback;
+    private Location mCurrentLocation;
+    private String mLastUpdateTime;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,6 +89,11 @@ public class MapActivity extends NMapActivity implements NMapView.OnMapStateChan
 
         // 지도에 대한 상태 변경 이벤트 연결
         mMapView.setOnMapStateChangeListener(this);
+
+        init();
+        startLocationUpdates();
+        showLastKnownLocation();
+
     }
 
     /**
@@ -63,7 +104,8 @@ public class MapActivity extends NMapActivity implements NMapView.OnMapStateChan
     @Override
     public void onMapInitHandler(NMapView mapview, NMapError errorInfo) {
         if (errorInfo == null) { // success
-            mMapController.setMapCenter(new NGeoPoint(126.978371, 37.5666091), 11);
+            mMapController.setMapCenter(new NGeoPoint(mCurrentLocation.getLongitude(), mCurrentLocation.getLatitude()), 11);
+            stopLocationUpdates();
         } else { // fail
             android.util.Log.e("NMAP", "onMapInitHandler: error=" + errorInfo.toString());
         }
@@ -73,13 +115,15 @@ public class MapActivity extends NMapActivity implements NMapView.OnMapStateChan
      * 지도 레벨 변경 시 호출되며 변경된 지도 레벨이 파라미터로 전달된다.
      */
     @Override
-    public void onZoomLevelChange(NMapView mapview, int level) {}
+    public void onZoomLevelChange(NMapView mapview, int level) {
+    }
 
     /**
      * 지도 중심 변경 시 호출되며 변경된 중심 좌표가 파라미터로 전달된다.
      */
     @Override
-    public void onMapCenterChange(NMapView mapview, NGeoPoint center) {}
+    public void onMapCenterChange(NMapView mapview, NGeoPoint center) {
+    }
 
     /**
      * 지도 애니메이션 상태 변경 시 호출된다.
@@ -87,8 +131,95 @@ public class MapActivity extends NMapActivity implements NMapView.OnMapStateChan
      * animState : ANIMATION_STATE_STARTED or ANIMATION_STATE_FINISHED
      */
     @Override
-    public void onAnimationStateChange(NMapView arg0, int animType, int animState) {}
+    public void onAnimationStateChange(NMapView arg0, int animType, int animState) {
+    }
 
     @Override
-    public void onMapCenterChangeFine(NMapView arg0) {}
+    public void onMapCenterChangeFine(NMapView arg0) {
+    }
+
+    private void init() {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mSettingsClient = LocationServices.getSettingsClient(this);
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                // location is received
+                mCurrentLocation = locationResult.getLastLocation();
+                mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+            }
+        };
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        mLocationSettingsRequest = builder.build();
+    }
+
+    private void startLocationUpdates() {
+        mSettingsClient
+                .checkLocationSettings(mLocationSettingsRequest)
+                .addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+                    @SuppressLint("MissingPermission")
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        Log.i(TAG, "All location settings are satisfied.");
+
+                        Toast.makeText(getApplicationContext(), "Started location updates!", Toast.LENGTH_SHORT).show();
+
+                        //noinspection MissingPermission
+                        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        int statusCode = ((ApiException) e).getStatusCode();
+                        switch (statusCode) {
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                Log.i(TAG, "Location settings are not satisfied. Attempting to upgrade " +
+                                        "location settings ");
+                                try {
+                                    // Show the dialog by calling startResolutionForResult(), and check the
+                                    // result in onActivityResult().
+                                    ResolvableApiException rae = (ResolvableApiException) e;
+                                    rae.startResolutionForResult(MapActivity.this, REQUEST_CHECK_SETTINGS);
+                                } catch (IntentSender.SendIntentException sie) {
+                                    Log.i(TAG, "PendingIntent unable to execute request.");
+                                }
+                                break;
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                String errorMessage = "Location settings are inadequate, and cannot be " +
+                                        "fixed here. Fix in Settings.";
+                                Log.e(TAG, errorMessage);
+
+                                Toast.makeText(MapActivity.this, errorMessage, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+    public void showLastKnownLocation() {
+        if (mCurrentLocation != null) {
+            Log.e(TAG, "Lat: " + mCurrentLocation.getLatitude() + ", Lng: " + mCurrentLocation.getLongitude());
+        } else {
+            Log.e(TAG, "Last known location is not available!");
+        }
+    }
+    public void stopLocationUpdates() {
+        // Removing location updates
+        mFusedLocationClient
+                .removeLocationUpdates(mLocationCallback)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(getApplicationContext(), "Location updates stopped!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 }
