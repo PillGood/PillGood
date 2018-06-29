1ff566c22bdb6a298d0623b91903b1a5a974dad5
gayeon1025 <gayeon1559@gmail.com>
2018-06-29 10:04:41 +0900

    MainActivity: add floating button and animation

--- app/src/main/java/com/example/pc/pillgood/MainActivity.java
+++ app/src/main/java/com/example/pc/pillgood/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.pc.pillgood;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
-
+    FloatingActionButton plusBtn, fastEnrollmentBtn, enrollmentBtn;
+    Boolean plusBtnIsClicked = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +23,48 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout =  findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+        //-----pop up floating btn
+        plusBtn = (FloatingActionButton) findViewById(R.id.mainPlusBtn);
+        fastEnrollmentBtn = (FloatingActionButton) findViewById(R.id.mainFastEnrollmentBtn);
+        enrollmentBtn = (FloatingActionButton) findViewById(R.id.mainEnrollmentBtn);
+        plusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation plusBtnAnim, fastEnrollmnetAnim, enrollmentAnim;
+                plusBtnAnim=AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_plusbtn);
+                if (plusBtnIsClicked) {
+                    fastEnrollmentBtn.setVisibility(View.INVISIBLE);
+                    enrollmentBtn.setVisibility(View.INVISIBLE);
+                    fastEnrollmnetAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_down_fastbtn);
+                    enrollmentAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_down_enrollmentbtn);
+                    plusBtnIsClicked = false;
+                }
+                else {
+                    fastEnrollmentBtn.setVisibility(View.VISIBLE);
+                    enrollmentBtn.setVisibility(View.VISIBLE);
+                    fastEnrollmnetAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_up_fastbtn);
+                    enrollmentAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_up_enrollmentbtn);
+                    plusBtnIsClicked = true;
+                }
+                plusBtn.startAnimation(plusBtnAnim);
+                fastEnrollmentBtn.startAnimation(fastEnrollmnetAnim);
+                enrollmentBtn.startAnimation(enrollmentAnim);
+            }
+        });
+
+        fastEnrollmentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        enrollmentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
